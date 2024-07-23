e55e6c6a1c887e026952c253ee213015352a64ee
KENT <kwilliam866@gmail.com>
2024-07-24 01:29:08 +0300

    numbers

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 29;
+let num4 = 5;
+let multiply = num1 * num2;
+console.log(multiply);
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+let mod = num3%num4
+console.log(mod);
+let max = Math.max(5, 15, 10, 20);
+console.log(max);
